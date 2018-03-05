39a08213c5463dbf6f7aa24ffdc5aced365018e8
wendytamara <anacarlavegam@gmail.com>
2018-03-04 22:08:10 -0500

    mejorando identacion

--- js/app.js
+++ js/app.js
@@ -17,61 +17,60 @@
     <section>
       <div className="biography">
       <p><strong>Grace Brewster Murray Hopper</strong> (<a href="https://en.wikipedia.org/wiki/Given_name#Name_at_birth">née</a> <strong>Murray</strong>; December 9, 1906 –
-     January 1, 1992) was an American <a href="https://en.wikipedia.org/wiki/Computer_scientist">computer scientist</a> and <a href="https://en.wikipedia.org/wiki/United_States_Navy">United States Navy</a>
-     <a href="https://en.wikipedia.org/wiki/Rear_admiral_(United_States)">rear admiral.[1]</a> One of the first programmers of the <a href="https://en.wikipedia.org/wiki/Harvard_Mark_I">Harvard Mark I computer</a>,
-      she was a pioneer of computer programming who invented one of the first
-       <a href="https://en.wikipedia.org/wiki/Compiler">compiler</a> related tools. She popularized the idea of machine-independent
-       programming languages, which led to the development of COBOL, an early
-        high-level programming language still in use today.</p>
+        January 1, 1992) was an American <a href="https://en.wikipedia.org/wiki/Computer_scientist">computer scientist</a> and <a href="https://en.wikipedia.org/wiki/United_States_Navy">United States Navy</a>
+        <a href="https://en.wikipedia.org/wiki/Rear_admiral_(United_States)">rear admiral.[1]</a> One of the first programmers of the <a href="https://en.wikipedia.org/wiki/Harvard_Mark_I">Harvard Mark I computer</a>,
+          she was a pioneer of computer programming who invented one of the first
+        <a href="https://en.wikipedia.org/wiki/Compiler">compiler</a> related tools. She popularized the idea of machine-independent
+          programming languages, which led to the development of COBOL, an early
+          high-level programming language still in use today.</p>
 
-        <p>Hopper had attempted to enlist in the Navy during <a href="https://en.wikipedia.org/wiki/World_War_II">World War II</a>, but
-      she was rejected by the military because she was 34 years of age and
-      too old to enlist. She instead joined the Navy Reserves. Hopper began
-       her computing career when she worked on the Harvard Mark I team that
-        was led byhttps://en.wikipedia.org/wiki/Howard_H._Aiken <a href="https://en.wikipedia.org/wiki/Howard_H._Aiken"> Howard H. Aiken</a>. In 1949, she joined the <a href="https://en.wikipedia.org/wiki/Eckert%E2%80%93Mauchly_Computer_Corporation">Eckert–Mauchly
-        Computer Corporation</a> and was part of the development team that
-         designed the <a href="https://en.wikipedia.org/wiki/UNIVAC_I">UNIVAC I</a> computer in 1944. It was at Eckert–Mauchly
+       <p>Hopper had attempted to enlist in the Navy during <a href="https://en.wikipedia.org/wiki/World_War_II">World War II</a>, but
+          she was rejected by the military because she was 34 years of age and
+          too old to enlist. She instead joined the Navy Reserves. Hopper began
+          her computing career when she worked on the Harvard Mark I team that
+          was led byhttps://en.wikipedia.org/wiki/Howard_H._Aiken <a href="https://en.wikipedia.org/wiki/Howard_H._Aiken"> Howard H. Aiken</a>. In 1949, she joined the <a href="https://en.wikipedia.org/wiki/Eckert%E2%80%93Mauchly_Computer_Corporation">Eckert–Mauchly
+          Computer Corporation</a> and was part of the development team that
+          designed the <a href="https://en.wikipedia.org/wiki/UNIVAC_I">UNIVAC I</a> computer in 1944. It was at Eckert–Mauchly
           that she began developing the compiler. She believed that computer
           code could be written in English by using a programming language
-           that was based on English words. The compiler would convert that
-           code into <a href="https://en.wikipedia.org/wiki/Machine_code">machine code</a> that would be understood by computers.
-            By 1952, Hopper finished her compiler, which was written for
-             the A-0 System programming language.[2][3][4][5]</p>
+          that was based on English words. The compiler would convert that
+          code into <a href="https://en.wikipedia.org/wiki/Machine_code">machine code</a> that would be understood by computers.
+          By 1952, Hopper finished her compiler, which was written for
+          the A-0 System programming language.[2][3][4][5]</p>
 
-             <p>In 1954, Eckert–Mauchly chose Hopper to lead their department
-               for automatic programming, and she led the release of some of
-                the first compiled languages like FLOW-MATIC. In 1959, she
- participated in the CODASYL consortium, which consulted Hopper
-to guide them in creating a machine-independent programming
-   language. This led to the COBOL language, which was inspired by her idea
-    of a language being based on English words. In 1966, she retired from
-    the Naval Reserve, but in 1967, the Navy recalled her to active duty.
-     She retired from the Navy in 1986 and found work as a consultant for
-the Digital Equipment Corporation, sharing her computing experiences.</p>
+       <p>In 1954, Eckert–Mauchly chose Hopper to lead their department
+         for automatic programming, and she led the release of some of
+         the first compiled languages like FLOW-MATIC. In 1959, she
+          participated in the CODASYL consortium, which consulted Hopper
+          to guide them in creating a machine-independent programming
+            language. This led to the COBOL language, which was inspired by her idea
+              of a language being based on English words. In 1966, she retired from
+              the Naval Reserve, but in 1967, the Navy recalled her to active duty.
+              She retired from the Navy in 1986 and found work as a consultant for
+          the Digital Equipment Corporation, sharing her computing experiences.</p>
 
-<h3>Carrer</h3>
-<ol>
-  <li>Early life and education</li>
-  <li>Career</li>
-    <ol>
-      <li>World War II</li>
-      <li>UNIVAC</li>
-      <li>COBOL</li>
-      <li>Standards</li>
-    </ol>
-  <li>Retirement</li>
-  <li>Post retirement</li>
-  <li>Anecdotes</li>
-  <li>Death</li>
-  <li>Dates of rank</li>
-  <li><a href="#">Awards and honors</a></li>
-</ol>
+      <h3>Carrer</h3>
+      <ol>
+        <li>Early life and education</li>
+        <li>Career</li>
+          <ol>
+            <li>World War II</li>
+            <li>UNIVAC</li>
+            <li>COBOL</li>
+            <li>Standards</li>
+          </ol>
+        <li>Retirement</li>
+        <li>Post retirement</li>
+        <li>Anecdotes</li>
+        <li>Death</li>
+        <li>Dates of rank</li>
+        <li><a href="#">Awards and honors</a></li>
+      </ol>
 
-<h2>Fhase</h2>
-<blockquote cite="http://www.mundifrases.com/frases-de/grace-murray-hopper/">
-“Un barco en el puerto es seguro, pero no es para eso para lo que se construyen las naves. Navegad en el mar y haced cosas nuevas.”
-</blockquote>
-
+        <h2>Fhase</h2>
+        <blockquote cite="http://www.mundifrases.com/frases-de/grace-murray-hopper/">
+        “Un barco en el puerto es seguro, pero no es para eso para lo que se construyen las naves. Navegad en el mar y haced cosas nuevas.”
+        </blockquote>
       </div>
     </section>
   );
@@ -119,20 +118,18 @@ the Digital Equipment Corporation, sharing her computing experiences.</p>
         <tr>
         <th>Rank	</th> <td><img src="https://en.wikipedia.org/wiki/File:US-O7_insignia.svg" alt=""/>Rear admiral (lower half)</td>
         </tr>
-        <tr>
-          <th>Awards	</th>
-          <td>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Meritorious_Service_Medal_ribbon.svg/33px-Meritorious_Service_Medal_ribbon.svg.png" alt=""/><a href="#"> Legion of Merit</a>
-          <br/>
+          <tr>
+            <th>Awards	</th>
+            <td>
+             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Meritorious_Service_Medal_ribbon.svg/33px-Meritorious_Service_Medal_ribbon.svg.png" alt=""/><a href="#"> Legion of Merit</a>
+            <br/>
               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Meritorious_Service_Medal_ribbon.svg/33px-Meritorious_Service_Medal_ribbon.svg.png" alt=""/><a href="#"> Legion of Merit</a>
-
+             <br/>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Meritorious_Service_Medal_ribbon.svg/33px-Meritorious_Service_Medal_ribbon.svg.png" alt=""/><a href="#"> Legion of Merit</a>
               <br/>
-                  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Meritorious_Service_Medal_ribbon.svg/33px-Meritorious_Service_Medal_ribbon.svg.png" alt=""/><a href="#"> Legion of Merit</a>
-
-                  <br/>
-                      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Meritorious_Service_Medal_ribbon.svg/33px-Meritorious_Service_Medal_ribbon.svg.png" alt=""/><a href="#"> Legion of Merit</a>
-          </td>
-        </tr>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Meritorious_Service_Medal_ribbon.svg/33px-Meritorious_Service_Medal_ribbon.svg.png" alt=""/><a href="#"> Legion of Merit</a>
+              </td>
+            </tr>
           </tbody>
         </table>
       </aside>
@@ -153,8 +150,6 @@ the Digital Equipment Corporation, sharing her computing experiences.</p>
       <h2>BIOGRAFIA</h2>
       <hr/>
       <p>Grace Brewster Murray, nació en Nueva York (EE. UU.). Grace fue una bisnieta de Alexander Russell, un almirante de la Armada de los Estados Unidos. Este fue su modelo y su héroe personal. También fue nieta de un ingeniero civil, John Van Horne. Sus padres fueron Walter Fletcher Murray, corredor de seguros y Mary Campbell Van Horne. Desde muy pequeña demostró aptitudes para las ciencias y la matemática. Recibió siempre el apoyo de su abuelo y de su padre para que las estudiara, pues quería que sus hijas tuvieran las mismas oportunidades que su hijo varón. También le atrajo mucho cualquier tipo de dispositivo mecánico, tanto fue así, que con 7 años desarmó todos los relojes de su casa para ver si podía entender cómo funcionaban. En 1930 se casa con Vincent Foster Hopper, un doctor en literatura en lenguaje inglés, que durante muchos años fue presidente del departamento de inglés de la universidad de Nueva York. Vincent y Grace se divorciaron en 1945 sin tener hijos.</p>
-
-
     </header>
   );
 
